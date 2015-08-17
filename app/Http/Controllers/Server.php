5e3c388e75ea6720dcd284bf61cc74005fc40804
Mitul <mitul@logisticinfotech.com>
2015-08-17 21:23:03 +0530

    ssh

--- app/Http/Controllers/Server.php
+++ app/Http/Controllers/Server.php
@@ -1,17 +1,20 @@
 <?php namespace App\Http\Controllers;
 
 use App\Http\Requests;
-use App\Http\Controllers\Controller;
-
 use Illuminate\Http\Request;
 use SSH;
-class Server extends Controller {
 
-	public function deploy(){
+class Server extends Controller
+{
+
+    public function deploy()
+    {
         SSH::into('production')->run(array(
             'cd /usr/share/nginx/html/clicspot',
             'git pull origin master'
-        ));
+        ), function ($line) {
+            echo $line . PHP_EOL; // outputs server feedback
+        });
     }
 
 }
