d03f89c651c539bbe56052e940f1c8a21aa5896f
Mitul <mitul@logisticinfotech.com>
2015-08-20 20:21:55 +0530

    ssh

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
     <div class="pull-right hidden-xs">
-        <b>Version</b> 2.0
+        <b>Version</b> 1.0
     </div>
-    <strong>Copyright &copy; 2015-2016 <a href="#">Nifty Trades</a> All rights reserved.</strong>
+    <strong>Copyright &copy; 2015-16 <a href="#">Clicspot</a> All rights reserved.</strong>
 </footer>
--- resources/views/vendor/vendorList.blade.php
+++ resources/views/vendor/vendorList.blade.php
@@ -1,10 +1,8 @@
 @extends('app')
-@push('styles')
-<link rel="stylesheet" href="{{ asset('plugins/datatables/dataTables.bootstrap.css')}}">
-@endpush
+        
 @section('content')
 
-<!-- Content Header (Page header) -->
+        <!-- Content Header (Page header) -->
 <section class="content-header">
     <h1>
         Vendors
@@ -22,88 +20,92 @@
             <div class="box">
                 <div class="box-header">
                     <h3 class="box-title">Vendor List</h3>
-                </div><!-- /.box-header -->
+                </div>
+                <!-- /.box-header -->
                 <div class="box-body">
-                    <table class="table table-bordered table-striped " id="users-table">
+                    <table class="table table-bordered table-striped dt-responsive" id="users-table" width="100%">
                         <thead>
-                            <tr>
-                                <th>Id</th>
-                                <th>Name</th>
-                                <th>Email</th>
-                                <th>Action</th>
-                                <th>Status</th>
-                            </tr>
+                        <tr>
+                            <th>Id</th>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Action</th>
+                            <th>Status</th>
+                        </tr>
                         </thead>
-                        <tbody></tbody>
+                        <tbody>
+
+                        </tbody>
                     </table>
-                </div><!-- /.box-body -->
-            </div><!-- /.box -->
-        </div><!-- /.col -->
-    </div><!-- /.row -->
+                </div>
+                <!-- /.box-body -->
+            </div>
+            <!-- /.box -->
+        </div>
+        <!-- /.col -->
+    </div>
+    <!-- /.row -->
 </section><!-- /.content -->
 </div><!-- /.content-wrapper -->
 @endsection
 @push('scripts')
-<!-- DataTables -->
-<script src="{{ asset('/plugins/datatables/jquery.dataTables.min.js')}}"></script>
-<script src="{{ asset('/plugins/datatables/dataTables.bootstrap.js')}}"></script>
+        <!-- DataTables -->
+<script type="text/javascript" src="{{ asset('/js/jquery.dataTables.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('/js/dataTables.bootstrap.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('/js/dataTables.responsive.js') }}"></script>
 <script>
 
-$(function () {
-    oTable = $('#users-table').DataTable({
-        processing: true,
-        serverSide: true,
-        "paging": true,
-        "lengthChange": false,
-        "searching": true,
-        "ordering": false,
-        "info": true,
-        ajax: 'vendorList',
-        columns: [
-            {data: 'id', name: 'id'},
-            {data: 'username', name: 'username'},
-            {data: 'email', name: 'email'},
-            {data: 'action', name: 'action'},
-            {data: 'isactivated', name: 'isactivated'}
-        ]
-    });
-
-
-    $(document).on('click', '#action', function () {
-        var $me = $(this);
-        swal({
-            title: "Are you sure?",
-            text: "",
-            type: "warning",
-            showCancelButton: true,
-            confirmButtonColor: "#DD6B55",
-            confirmButtonText: "Yes, Active it!",
-            cancelButtonText: "No, cancel!",
-            closeOnConfirm: false,
-            closeOnCancel: false
-        },
-        function (isConfirm) {
-            if (isConfirm) {
-                var id = jQuery($me).attr('val');
-                jQuery.ajax({
-                    url: 'vendorAction/' + id,
-                    type: 'GET',
-                    success: function (result) {
-                        if (result) {
-                            oTable.draw();
-                            swal("success!", "User account activated successfully.", "success");
-                        } else {
-                            alert('false');
-                            swal("ohh snap!", "something went wrong", "error");
-                        }
-
-                    }
-                });
-            } else {
-                swal("Cancelled", "User action is cancelled ", "error");
-               //return true;
-            }
+    $(function () {
+        oTable = $('#users-table').DataTable({
+            processing: true,
+            serverSide: true,
+            responsive: true,
+            ajax: 'vendorList',
+            columns: [
+                {data: 'id', name: 'id'},
+                {data: 'username', name: 'username'},
+                {data: 'email', name: 'email'},
+                {data: 'action', name: 'action', orderable: false, searchable: false},
+                {data: 'isactivated', name: 'isactivated', orderable: false, searchable: false}
+            ]
         });
+
+
+        $(document).on('click', '#action', function () {
+            var $me = $(this);
+            swal({
+                        title: "Are you sure?",
+                        text: "",
+                        type: "warning",
+                        showCancelButton: true,
+                        confirmButtonColor: "#DD6B55",
+                        confirmButtonText: "Yes, Active it!",
+                        cancelButtonText: "No, cancel!",
+                        closeOnConfirm: false,
+                        closeOnCancel: false
+                    },
+                    function (isConfirm) {
+                        if (isConfirm) {
+                            var id = jQuery($me).attr('val');
+                            jQuery.ajax({
+                                url: 'vendorAction/' + id,
+                                type: 'GET',
+                                success: function (result) {
+                                    if (result) {
+                                        oTable.draw();
+                                        swal("success!", "User account activated successfully.", "success");
+                                    } else {
+                                        alert('false');
+                                        swal("ohh snap!", "something went wrong", "error");
+                                    }
+
+                                }
+                            });
+                        } else {
+                            swal("Cancelled", "User action is cancelled ", "error");
+                            //return true;
+                        }
+                    });
 //        var flag = confirm('Are Sure to confirm the user');
 //        if (flag) {
 //            var id = jQuery(this).attr('val');
@@ -124,9 +126,9 @@ $(function () {
 //        } else {
 //            return false;
 //        }
-        return false;
+            return false;
+        });
     });
-});
 </script>
 
 @endpush
