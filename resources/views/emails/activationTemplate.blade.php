e25dad83a77e3fe2d0c87031e74ab663afdf892f
Mitul <mitul@logisticinfotech.com>
2015-08-20 20:41:24 +0530

    ssh

--- resources/views/emails/activationTemplate.blade.php
+++ resources/views/emails/activationTemplate.blade.php
@@ -1,5 +1,7 @@
 <html>
 <body>
-Thank you for registering for ClickSpot WIFI , please click the following link to confirm your account and access the website: <a style="" href="{{ Config::get('app.url') }}verify/{{ $confirmationCode }}" target="_blank">{{ Config::get('app.url') }}verify/{{ $confirmationCode }}</a>
+Thank you for registering for ClickSpot WIFI , please click the following link to confirm your account and access the
+website: <a style="" href="{{ url("/") }}/verify/{{ $confirmationCode }}" target="_blank">{{ url("/") }}
+    /verify/{{ $confirmationCode }}</a>
 </body>
 </html>
