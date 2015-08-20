7e49061edabdf8fc81cf23fc1ffac3f6d921e566
Mitul <mitul@logisticinfotech.com>
2015-08-20 22:16:27 +0530

    ssh

--- resources/app.blade.php
+++ resources/app.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>{{ $projectTitle or "AdminLTE 2" }} | Dashboard</title>
+    <title>ClicSpot | Dashboard</title>
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
     <!-- Bootstrap 3.3.2 -->
     <link href="{{ asset('/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css"/>
