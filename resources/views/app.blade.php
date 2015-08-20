93d89f689d6b25856ef4dc056a0055afad47e397
Mitul <mitul@logisticinfotech.com>
2015-08-20 22:20:53 +0530

    ssh

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>{{ $projectTitle or "AdminLTE 2" }} | Dashboard</title>
+    <title>Clicspot | Dashboard</title>
     <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
     <!-- Bootstrap 3.3.2 -->
     <link href="{{ asset('/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css"/>
@@ -31,7 +31,7 @@
     <![endif]-->
     @stack('styles')
 </head>
-<body class="skin-blue sidebar-mini">
+<body class="skin-black sidebar-mini">
 <div class="wrapper">
     @include('includes.header')
     @include('includes.sidebar')
