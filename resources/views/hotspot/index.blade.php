58bfd77bbe21cdf2abac96795ef36da247c571d1
Mitul <mitul@logisticinfotech.com>
2015-08-23 01:51:27 +0530

    ssh

--- resources/views/hotspot/index.blade.php
+++ resources/views/hotspot/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-<!-- Content Header (Page header) -->
+        <!-- Content Header (Page header) -->
 <section class="content-header">
     <h1>
         Hotspot
@@ -22,21 +22,20 @@
                     <h3 class="box-title">Hotspot List</h3>
                     <a href="{{url('hotspot/create')}}" class="btn btn-info pull-right">Add Hotspot</a>
                 </div>
-                
+
                 <!-- /.box-header -->
                 <div class="box-body">
-                    
+
                     <table class="table table-bordered table-striped dt-responsive" id="hotspot-table" width="100%">
                         <thead>
-                            <tr>
-                                <th>Id</th>
-                                <th>Name</th>
-                                <th>MAC Address</th>
-                                <th>Public IP</th>
-                                <th>Status</th>
-                                <th>Edit</th>
-                                <th>Delete</th>
-                            </tr>
+                        <tr>
+                            <th>Name</th>
+                            <th>MAC Address</th>
+                            <th>Public IP</th>
+                            <th>Status</th>
+                            <th>Edit</th>
+                            <th>Delete</th>
+                        </tr>
                         </thead>
                         <tbody>
 
@@ -53,73 +52,72 @@
 </section><!-- /.content -->
 @endsection
 @push('scripts')
-<!-- DataTables -->
+        <!-- DataTables -->
 <script type="text/javascript" src="{{ asset('/js/jquery.dataTables.min.js') }}"></script>
 <script type="text/javascript" src="{{ asset('/js/dataTables.bootstrap.min.js') }}"></script>
 <script type="text/javascript" src="{{ asset('/js/dataTables.responsive.js') }}"></script>
 <script>
 
-$(function () {
-    oTable = $('#hotspot-table').DataTable({
-        processing: true,
-        serverSide: true,
-        responsive: true,
-        ajax: 'hotspot',
-        columns: [
-            {data: 'id', name: 'id'},
-            {data: 'shortname', name: 'shortname'},
-            {data: 'nasidentifier', name: 'nasidentifier'},
-            {data: 'publicip', name: 'publicip'},
-            {data: 'status', name: 'status'},
-            {data: 'edit', name: 'edit', orderable: false, searchable: false},
-            {data: 'delete', name: 'delete', orderable: false, searchable: false}
-        ]
-    });
-
-
-    $(document).on('click', '#delete', function () {
-        var $me = $(this);
-        swal({
-            title: "Are you sure?",
-            text: "",
-            type: "warning",
-            showCancelButton: true,
-            confirmButtonColor: "#DD6B55",
-            confirmButtonText: "Yes, Delete it!",
-            cancelButtonText: "No, cancel!",
-            closeOnConfirm: false,
-            closeOnCancel: false
-        },
-        function (isConfirm) {
-            if (isConfirm) {
-                var id = jQuery($me).attr('val');
-                var token = jQuery($me).attr('data-token');
-                
-                jQuery.ajax({
-                    url: 'hotspot/' + id,
-                    type: 'DELETE',
-                    data: {
-                        "_token": token
-                    },
-                    success: function (result) {
-                        if (result.success) {
-                            swal("success!", "Hotspot deleted successfully.", "success");
-                            oTable.draw();
-                        } else {
-                            alert('false');
-                            swal("ohh snap!", "something went wrong", "error");
-                        }
-
-                    }
-                });
-            } else {
-                swal("Cancelled", "Hotspot delete is cancelled ", "error");
-                //return true;
-            }
+    $(function () {
+        oTable = $('#hotspot-table').DataTable({
+            processing: true,
+            serverSide: true,
+            responsive: true,
+            ajax: 'hotspot',
+            columns: [
+                {data: 'shortname', name: 'shortname'},
+                {data: 'nasidentifier', name: 'nasidentifier'},
+                {data: 'publicip', name: 'publicip'},
+                {data: 'status', name: 'status'},
+                {data: 'edit', name: 'edit', orderable: false, searchable: false},
+                {data: 'delete', name: 'delete', orderable: false, searchable: false}
+            ]
         });
-        return false;
+
+
+        $(document).on('click', '#delete', function () {
+            var $me = $(this);
+            swal({
+                        title: "Are you sure?",
+                        text: "",
+                        type: "warning",
+                        showCancelButton: true,
+                        confirmButtonColor: "#DD6B55",
+                        confirmButtonText: "Yes, Delete it!",
+                        cancelButtonText: "No, cancel!",
+                        closeOnConfirm: false,
+                        closeOnCancel: false
+                    },
+                    function (isConfirm) {
+                        if (isConfirm) {
+                            var id = jQuery($me).attr('val');
+                            var token = jQuery($me).attr('data-token');
+
+                            jQuery.ajax({
+                                url: 'hotspot/' + id,
+                                type: 'DELETE',
+                                data: {
+                                    "_token": token
+                                },
+                                success: function (result) {
+                                    if (result.success) {
+                                        swal("success!", "Hotspot deleted successfully.", "success");
+                                        oTable.draw();
+                                    } else {
+                                        alert('false');
+                                        swal("ohh snap!", "something went wrong", "error");
+                                    }
+
+                                }
+                            });
+                        } else {
+                            swal("Cancelled", "Hotspot delete is cancelled ", "error");
+                            //return true;
+                        }
+                    });
+            return false;
+        });
     });
-});
 </script>
 
 @endpush
