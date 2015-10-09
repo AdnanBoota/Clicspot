613639e7b79c4e299a39038c3dd46ac7d6429c6b
mitulgadhia <gadhia.mitul41@gmail.com>
2015-10-10 04:51:02 +0530

    ssh

--- resources/views/hotspot/create.blade.php
+++ resources/views/hotspot/create.blade.php
@@ -54,21 +54,21 @@
 </section><!-- /.content -->
 @endsection
 @push('scripts')
-<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false&libraries=places&language=en-AU"></script>
+<script type="text/javascript"
+        src="http://maps.google.com/maps/api/js?sensor=false&libraries=places&language=en-AU"></script>
 <script src="{{ asset('/plugins/input-mask/inputmask.js') }}" type="text/javascript"></script>
 <script src="{{ asset('/plugins/input-mask/jquery.inputmask.js') }}" type="text/javascript"></script>
 <script src="{{ asset('/plugins/input-mask/jquery.inputmask.extensions.js') }}" type="text/javascript"></script>
-<script type="text/javascript">
+<script>
     var map;
     var marker;
-    window.onload = function () {
+    function initialize() {
         var defaultLat = '<?php echo count($hotspotDetails) > 0 ? $hotspotDetails->latitude : 22.3000 ?>';
         var defaultLang = '<?php echo count($hotspotDetails) > 0 ? $hotspotDetails->longitude : 70.7833 ?>';
 
         var myLatlng = new google.maps.LatLng(defaultLat, defaultLang);
         var mapOptions = {
             zoom: 22
-            //        mapTypeId: google.maps.MapTypeId.SATELLITE
         };
         map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
         marker = new google.maps.Marker({
@@ -76,6 +76,78 @@
             draggable: true,
             map: map
         });
+
+        function handleNoGeolocation(errorFlag) {
+            if (errorFlag) {
+                var content = 'Error: The Geolocation service failed.';
+            } else {
+                var content = 'Error: Your browser doesn\'t support geolocation.';
+            }
+
+            var options = {
+                map: map,
+                position: new google.maps.LatLng(60, 105),
+                content: content
+            };
+
+            map.setCenter(options.position);
+            marker = new google.maps.Marker({
+                position: options.position,
+                map: map,
+                draggable: true
+            });
+
+        }
+
+        // get places auto-complete when user type in location-text-box
+        var input = $('#address');
+
+
+        var autocomplete = new google.maps.places.Autocomplete(input);
+        autocomplete.bindTo('bounds', map);
+
+        var infowindow = new google.maps.InfoWindow();
+        marker = new google.maps.Marker({
+            map: map,
+            anchorPoint: new google.maps.Point(0, -29),
+            draggable: true
+        });
+
+        google.maps.event.addListener(autocomplete, 'place_changed', function () {
+            infowindow.close();
+            marker.setVisible(false);
+            var place = autocomplete.getPlace();
+            if (!place.geometry) {
+                return;
+            }
+
+
+            // If the place has a geometry, then present it on a map.
+            if (place.geometry.viewport) {
+                map.fitBounds(place.geometry.viewport);
+            } else {
+                map.setCenter(place.geometry.location);
+                map.setZoom(17); // Why 17? Because it looks good.
+            }
+            marker.setIcon(/** @type {google.maps.Icon} */ ({
+                url: place.icon,
+                size: new google.maps.Size(71, 71),
+                origin: new google.maps.Point(0, 0),
+                anchor: new google.maps.Point(17, 34),
+                scaledSize: new google.maps.Size(35, 35)
+            }));
+            marker.setPosition(place.geometry.location);
+            marker.setVisible(true);
+
+            var address = '';
+            if (place.address_components) {
+                address = [
+                    (place.address_components[0] && place.address_components[0].short_name || ''), (place.address_components[1] && place.address_components[1].short_name || ''), (place.address_components[2] && place.address_components[2].short_name || '')
+                ].join(' ');
+            }
+
+        });
+
         google.maps.event.addListener(marker, 'dragend', function (evt) {
 
             $("[name=latitude]").val(evt.latLng.lat());
@@ -92,16 +164,53 @@
             });
         });
 
-        map.setCenter(marker.position);
-        marker.setMap(map);
-
-        var autocomplete = new google.maps.places.Autocomplete($("#address")[0], {});
-        autocomplete.bindTo('bounds', map);
-        google.maps.event.addListener(autocomplete, 'place_changed', function() {
-            var place = autocomplete.getPlace();
-            console.log(place.address_components);
-        });
     }
+
+    google.maps.event.addDomListener(window, 'load', initialize);
+</script>
+<script type="text/javascript">
+    {{--var map;--}}
+    {{--var marker;--}}
+    {{--window.onload = function () {--}}
+        {{--var defaultLat = '<?php echo count($hotspotDetails) > 0 ? $hotspotDetails->latitude : 22.3000 ?>';--}}
+        {{--var defaultLang = '<?php echo count($hotspotDetails) > 0 ? $hotspotDetails->longitude : 70.7833 ?>';--}}
+
+        {{--var myLatlng = new google.maps.LatLng(defaultLat, defaultLang);--}}
+        {{--var mapOptions = {--}}
+            {{--zoom: 22--}}
+        {{--};--}}
+        {{--map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);--}}
+        {{--marker = new google.maps.Marker({--}}
+            {{--position: myLatlng,--}}
+            {{--draggable: true,--}}
+            {{--map: map--}}
+        {{--});--}}
+        {{--google.maps.event.addListener(marker, 'dragend', function (evt) {--}}
+
+            {{--$("[name=latitude]").val(evt.latLng.lat());--}}
+            {{--$("[name=longitude]").val(evt.latLng.lng());--}}
+        {{--});--}}
+
+        {{--google.maps.event.addListener(map, 'click', function (evt) {--}}
+            {{--marker.setMap(null);--}}
+            {{--$("[name=latitude]").val(evt.latLng.lat());--}}
+            {{--$("[name=longitude]").val(evt.latLng.lng());--}}
+            {{--marker = new google.maps.Marker({--}}
+                {{--position: evt.latLng,--}}
+                {{--map: map--}}
+            {{--});--}}
+        {{--});--}}
+
+        {{--map.setCenter(marker.position);--}}
+        {{--marker.setMap(map);--}}
+
+        {{--var autocomplete = new google.maps.places.Autocomplete($("#address")[0], {});--}}
+        {{--autocomplete.bindTo('bounds', map);--}}
+        {{--google.maps.event.addListener(autocomplete, 'place_changed', function () {--}}
+            {{--var place = autocomplete.getPlace();--}}
+            {{--console.log(place.address_components);--}}
+        {{--});--}}
+    {{--}--}}
 
     jQuery(document).ready(function () {
         $('#nasidentifier').inputmask("mac");
