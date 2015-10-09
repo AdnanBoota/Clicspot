a9e0e13af24418fc863f0197219b000142cedfed
mitulgadhia <gadhia.mitul41@gmail.com>
2015-10-10 04:38:04 +0530

    ssh

--- resources/views/hotspot/create.blade.php
+++ resources/views/hotspot/create.blade.php
@@ -60,7 +60,7 @@
 <script src="{{ asset('/plugins/input-mask/jquery.inputmask.extensions.js') }}" type="text/javascript"></script>
 <script>
     var autocomplete = new google.maps.places.Autocomplete($("#address")[0], {});
-
+    autocomplete.bindTo('bounds', map);
     google.maps.event.addListener(autocomplete, 'place_changed', function() {
         var place = autocomplete.getPlace();
         console.log(place.address_components);
@@ -76,8 +76,8 @@
 
         var myLatlng = new google.maps.LatLng(defaultLat, defaultLang);
         var mapOptions = {
-            zoom: 22,
-            center: {lat: defaultLat, lng: defaultLang}
+            zoom: 22
+            //        mapTypeId: google.maps.MapTypeId.SATELLITE
         };
         map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
         marker = new google.maps.Marker({
@@ -127,46 +127,46 @@
                 $(element).parents('.form-group').addClass('has-success');
             }
         });
-        $("#address").blur(function () {
-            var address = $(this).val();
-            $.ajax({
-                url: "http://maps.googleapis.com/maps/api/geocode/json?address=" + address + "&sensor=false",
-                type: "POST",
-                success: function (res) {
-                    console.log(res.results[0].geometry.location.lat);
-                    console.log(res.results[0].geometry.location.lng);
-                    $("[name=latitude]").val(res.results[0].geometry.location.lat);
-                    $("[name=longitude]").val(res.results[0].geometry.location.lng);
-
-                    marker.setMap(null);
-                    marker = new google.maps.Marker({
-                        position: res.results[0].geometry.location,
-                        draggable: true,
-                        map: map
-                    });
-                    map.setCenter(marker.position);
-                    marker.setMap(map);
-
-                    google.maps.event.addListener(marker, 'dragend', function (evt) {
-                        //                console.log(geocodePosition(marker.getPosition()));
-                        $("[name=latitude]").val(evt.latLng.lat());
-                        $("[name=longitude]").val(evt.latLng.lng());
-                    });
-
-                    google.maps.event.addListener(map, 'click', function (evt) {
-                        console.log(evt);
-                        marker.setMap(null);
-                        $("[name=latitude]").val(evt.latLng.lat());
-                        $("[name=longitude]").val(evt.latLng.lng());
-                        marker = new google.maps.Marker({
-                            position: evt.latLng,
-                            draggable: true,
-                            map: map
-                        });
-                    });
-                }
-            });
-        });
+//        $("#address").blur(function () {
+//            var address = $(this).val();
+//            $.ajax({
+//                url: "http://maps.googleapis.com/maps/api/geocode/json?address=" + address + "&sensor=false",
+//                type: "POST",
+//                success: function (res) {
+//                    console.log(res.results[0].geometry.location.lat);
+//                    console.log(res.results[0].geometry.location.lng);
+//                    $("[name=latitude]").val(res.results[0].geometry.location.lat);
+//                    $("[name=longitude]").val(res.results[0].geometry.location.lng);
+//
+//                    marker.setMap(null);
+//                    marker = new google.maps.Marker({
+//                        position: res.results[0].geometry.location,
+//                        draggable: true,
+//                        map: map
+//                    });
+//                    map.setCenter(marker.position);
+//                    marker.setMap(map);
+//
+//                    google.maps.event.addListener(marker, 'dragend', function (evt) {
+//                        //                console.log(geocodePosition(marker.getPosition()));
+//                        $("[name=latitude]").val(evt.latLng.lat());
+//                        $("[name=longitude]").val(evt.latLng.lng());
+//                    });
+//
+//                    google.maps.event.addListener(map, 'click', function (evt) {
+//                        console.log(evt);
+//                        marker.setMap(null);
+//                        $("[name=latitude]").val(evt.latLng.lat());
+//                        $("[name=longitude]").val(evt.latLng.lng());
+//                        marker = new google.maps.Marker({
+//                            position: evt.latLng,
+//                            draggable: true,
+//                            map: map
+//                        });
+//                    });
+//                }
+//            });
+//        });
     });
 
 
