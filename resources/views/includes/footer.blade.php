3b0d666038d6f638502748c09e9c1995e601f8e8
mitulgadhia <gadhia.mitul41@gmail.com>
2015-10-11 05:25:20 +0530

    ssh

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,6 +1,6 @@
-<footer class="main-footer">
+<div class="main-footer">
     <div class="pull-right hidden-xs">
         <b>Version</b> 1.0
     </div>
     <strong>Copyright &copy; 2015-16 <a href="#">Clicspot</a> All rights reserved.</strong>
-</footer>
+</div>
