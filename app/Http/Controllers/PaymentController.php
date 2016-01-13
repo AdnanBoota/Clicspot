563b22d8f769d6e9a56754d9812cf7d72f215f4a
pritesh13 <bindeshpandya@hotmail.com>
2016-01-13 15:36:12 +0530

    Payment Design & Devlopment

---
+++ app/Http/Controllers/PaymentController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use App\SubscriptionHistory;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\View;
+use DB;
+use Auth;
+use Response;
+use Carbon;
+
+class PaymentController extends Controller {
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
+    public function __construct() {
+        $this->middleware('auth');
+        View::share('projectTitle', 'ClicSpot');
+    }
+
+    public function index() {
+        $adminId = Auth::user()->id;
+        $nextBillingDate = "User is not Subscribed";
+        $paymentDetails = SubscriptionHistory::where('adminid', "=", $adminId)->orderBy('id', 'desc')->first();
+        if ($paymentDetails) {
+            $nextBillingDate = Carbon\Carbon::parse($paymentDetails[0]->nextpaymentdate)->format('d/m/Y');
+        }
+        return view('payment.index', compact('nextBillingDate'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function create() {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @return Response
+     */
+    public function store() {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function show($id) {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function edit($id) {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function update($id) {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function destroy($id) {
+        //
+    }
+
+}
