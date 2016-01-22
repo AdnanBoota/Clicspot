e5d9fe633fe17971373c6156555b01879433f2f4
pritesh13 <bindeshpandya@hotmail.com>
2016-01-22 14:31:15 +0530

    email campaign

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -1,31 +1,40 @@
-<?php namespace App\Console;
+<?php
+
+namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Auth;
+use App\User;
 
 class Kernel extends ConsoleKernel {
 
-	/**
-	 * The Artisan commands provided by your application.
-	 *
-	 * @var array
-	 */
-	protected $commands = [
-		'App\Console\Commands\Inspire',
-	];
+    /**
+     * The Artisan commands provided by your application.
+     *
+     * @var array
+     */
+    protected $commands = [
+        'App\Console\Commands\Inspire',
+    ];
 
-	/**
-	 * Define the application's command schedule.
-	 *
-	 * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
-	 * @return void
-	 */
-	protected function schedule(Schedule $schedule)
-	{
+    /**
+     * Define the application's command schedule.
+     *
+     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @return void
+     */
+    protected function schedule(Schedule $schedule) {
+        $schedule->call(function () {
+        $userId['userId'] = Auth::user()->id;
+        $userId['templateName'] = "temp";
+        Mail::send('email.emailTemplate', $userId, function ($message) {
+        $message->to('bindeshpandya@hotmail.com', 'example_name')->subject('Welcome!');
+        })->daily();
         $schedule->call('HotspotController@sendmailTestCron')->everyMinute();
 
 //		$schedule->command('inspire')
 //				 ->hourly();
-	}
-
-}
+        }
+    }
+    
