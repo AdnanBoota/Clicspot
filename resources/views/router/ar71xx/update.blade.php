8ce74ae04d13d12debe4aed586af975c5fde48f0
mitulgadhia <gadhia.mitul41@gmail.com>
2016-01-22 20:10:49 +0530

    ssh

--- resources/views/router/ar71xx/update.blade.php
+++ resources/views/router/ar71xx/update.blade.php
@@ -7,8 +7,9 @@
 
 @endif
 WLANMAC=$(ifconfig br-lan | awk '/HWaddr/ { print $5 }' | sed 's/:/-/g')
+SSID=$(uci get wireless.@wifi-iface[0].ssid)
 
-echo "* * * * * wget '{{ url('api/v1/ar71xx/update') }}/$WLANMAC?configversion=25112015&ssid={{isset($hotspot->ssid)?$hotspot->ssid:"Clicspot"}}' -O /tmp/cloudconfig.sh;sh /tmp/cloudconfig.sh;rm /tmp/cloudconfig.sh" > /tmp/crontab
+echo "* * * * * wget '{{ url('api/v1/ar71xx/update') }}/$WLANMAC?configversion=25112015&ssid=$SSID' -O /tmp/cloudconfig.sh;sh /tmp/cloudconfig.sh;rm /tmp/cloudconfig.sh" > /tmp/crontab
 crontab /tmp/crontab
 rm /tmp/crontab
 
