b31e32cd9f213343a002f726c05743a295e6ce9b
pritesh13 <bindeshpandya@hotmail.com>
2016-01-22 12:36:52 +0530

    email campaign

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -22,8 +22,10 @@ class Kernel extends ConsoleKernel {
 	 */
 	protected function schedule(Schedule $schedule)
 	{
-		$schedule->command('inspire')
-				 ->hourly();
+        $schedule->call('HotspotController@sendmailTestCron')->everyMinute();
+
+//		$schedule->command('inspire')
+//				 ->hourly();
 	}
 
 }
