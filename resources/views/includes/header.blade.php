757c6d5ae28a14f44ad9c6908af01d6db5c6a57f
pritesh13 <garachankur@yahoo.in>
2016-02-17 12:13:39 +0530

    changes language

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -16,12 +16,27 @@
             <span class="sr-only">Toggle navigation</span>
         </a>
         <ul class="headnav">
-            <li><a href="#"><span>Activity</span></a></li>
-            <li><a href="#"><span>Notifications</span><span class="notidtl">19</span></a></li>
+            <li><a href="#"><span>{{ Lang::get('auth.activity') }}</span></a></li>
+            <li><a href="#"><span>{{ Lang::get('auth.notification') }}</span><span class="notidtl">19</span></a></li>
         </ul>
         <div class="navbar-custom-menu userprofile">
             <ul class="nav navbar-nav">
                 
+                <li>
+                    <div class="language">
+<!--                {{  App::getLocale() }} -->
+                <form action="{{ URL::route('language')  }}" method="post" id="language">
+                    <input name="_token" type="hidden" value="{{ csrf_token() }}">
+                    <input name="page" type="hidden" value="login">
+                    <select name="locale" id="locale">
+                        <option value="en" {{  (App::getLocale()=='en') ? 'selected' : '' }}>English</option>
+                        <option value="fr" {{  (App::getLocale()=='fr') ? 'selected' : '' }} >French</option>
+                    </select>
+                   
+                    
+                </form>
+            </div>
+                </li>
                 <li class="dropdown user user-menu">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                         <img src="{{ asset('dist/img/user2-160x160.jpg') }}" class="user-image" alt="User Image"/>
@@ -35,23 +50,35 @@
 
                             <p>
                                 {{ Auth::user()->username }}
-                                <small>Member
-                                    since {{ Carbon\Carbon::parse(\Illuminate\Support\Facades\Auth::user()->created_at)->format('d M Y') }}
+                                <small>{{ Lang::get('auth.member') }}
+                                    {{ Lang::get('auth.since') }} {{ Carbon\Carbon::parse(\Illuminate\Support\Facades\Auth::user()->created_at)->format('d M Y') }}
                                 </small>
                             </p>
                         </li>
                         <!-- Menu Footer-->
                         <li class="user-footer">
                             <div class="pull-left">
-                                <a href="#" class="btn btn-default btn-flat">Profile</a>
+                                <a href="#" class="btn btn-default btn-flat">{{ Lang::get('auth.profile') }}</a>
                             </div>
                             <div class="pull-right">
-                                <a href="{{ url('/auth/logout') }}" class="btn btn-default btn-flat">Sign out</a>
+                                <a href="{{ url('/auth/logout') }}" class="btn btn-default btn-flat">{{ Lang::get('auth.signout') }}</a>
                             </div>
                         </li>
                     </ul>
                 </li>
             </ul>
         </div>
+        
     </nav>
+    @push('scripts')
+    <script>
+    $(function(){
+           $("#locale").change(function(){
+               
+           $("#language").submit();
+            });
+           
+        });
+    </script>
+    @endpush
 </header>
