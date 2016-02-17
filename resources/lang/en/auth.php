299ea2a97e2d2984e65489a513ec26f52f039211
pritesh13 <garachankur@yahoo.in>
2016-02-17 12:14:47 +0530

    english  language content

---
+++ resources/lang/en/auth.php
@@ -0,0 +1,63 @@
+<?php
+
+return [
+
+	/*
+	|--------------------------------------------------------------------------
+	| Pagination Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are used by the paginator library to build
+	| the simple pagination links. You are free to change them to anything
+	| you want to customize your views to better match your application.
+	|
+	*/
+
+	'loginTitle' => 'Sign in to start your session',
+    'donthaveaccount'=>'Don\'t have an account?',
+    'signup'=>'Sign Up',
+    'login'=>'Login',
+    'lostpassword'=>'Lost your password?',
+    'email'=>'Email',
+    'password'=>'Password',
+    'haveaccount'=>'Already have an account?',
+    'signin'=>'Sign in',
+    'retypepass'=>'Retype Password',
+    'fullnm'=>'Full Name',
+    'business'=>'Buisness Name',
+    'address'=>'Adress',
+    'city'=>'City',
+    'zipcode'=>'Zip code',
+    'country'=>'Country',
+    'phone'=>'Phone number',
+    'SIREN'=>'SIREN',
+    'vat'=>'N° VAT',
+    'profileinfo'=>'Profile Infos',
+    'businessinfo'=>'Business Infos',
+    'paymentinfo'=>'Payment Infos',
+    'step2'=>'GO TO STEP 2',
+    'step3'=>'GO TO STEP 3',
+    'paragraph'=>'To finalize your acount, you will be redirected to our Direct Bank provider GoCardLess. You will need your IBAN information.',
+    'cardless'=>'GO TO GoCardLess',
+    'agree'=>'By signing up you agree to our terms & conditions',
+    'activity'=>'Activity',
+    'notification'=>'Notification',
+    'signout'=>'Sign out',
+    'profile'=>'Profile',
+    'member'=>'Member',
+    'since'=>'Since',
+    'dashboard'=>'Dashboard',
+    'hotspot'=>'Hotspot',
+    'campaign'=>'Campaign ( Captival Portal )',
+    'user'=>'Users',
+    'emails'=>'Emails',
+    'payment'=>'Payment',
+    'statistics'=>'Statistics',
+    'routerstatistics'=>'Router Statistics',
+    'routerstatus'=>'Router Status',
+    'amountconnection'=>'Amount of connections ( Number of connections )',
+    'memberstatistics'=>'Member Statistics ( Users Statistics )',
+    'latestmember'=>'Latest members ( Latest Users )',
+    'reviews'=>'Reviews'
+
+];
