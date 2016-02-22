73a3ff3a26499e070a85e64c0da8bae1859388b1
pritesh13 <garachankur@yahoo.in>
2016-02-22 19:03:54 +0530

    add lang code to transper to other lang

--- resources/views/email/_form.blade.php
+++ resources/views/email/_form.blade.php
@@ -4,27 +4,27 @@
 
     <div class="col-md-9 stepform1 {{isset($campaignData->currentForm) ? '':'currentForm' }}{{isset($campaignData->currentForm) && $campaignData->currentForm == '1' ? 'currentForm':''}}" id="1">
         <div class="form-group formrow">
-            <label>Campaign Name</label><a href="#"><i class="anyque"></i></a>
-            {!!  Form::text('campaignName', null, array('id'=>'campaignName','placeholder'=>'MyCampaign','class'=>'form-control','tabindex'=>'1')) !!}
+            <label>{{ Lang::get('auth.campaignnm')}}</label><a href="#"><i class="anyque"></i></a>
+            {!!  Form::text('campaignName', null, array('id'=>'campaignName','placeholder'=>Lang::get("auth.mycampaign"),'class'=>'form-control','tabindex'=>'1')) !!}
             
         </div>
         <div class="form-group formrow">
-            <label>Sender Email</label><a href="#"><i class="anyque"></i></a>
+            <label>{{ Lang::get('auth.sendermail')}}</label><a href="#"><i class="anyque"></i></a>
             {!!  Form::text('senderEmail', $email, array('id'=>'senderEmail','placeholder'=>'Email Address','class'=>'form-control','readonly','tabindex'=>'2')) !!}
     
         </div>
         <div class="form-group formrow">
-            <label>From Name</label><a href="#"><i class="anyque"></i></a>
+            <label>{{ Lang::get('auth.Formname')}}</label><a href="#"><i class="anyque"></i></a>
             {!!  Form::text('fromName', $username, array('id'=>'senderName','placeholder'=>'Your Name','class'=>'form-control','readonly','tabindex'=>'3')) !!}
     
         </div>
     </div>
     <div class="col-md-10 stepform2 {{ isset($campaignData->currentForm) && $campaignData->currentForm == '2' ? 'currentForm':''}}" id="2">
         <div class="form-group formrow">
-            <label>Campaign Name</label>
-            <p>Create a mobile-friendly email that looks great on any device or screen size.</p>
+            <label>{{ Lang::get('auth.campaignnm')}}</label>
+            <p>{{ Lang::get('auth.createmobile') }}</p>
             <!--<a href="{{url('emails/create')}}" class="crtbtn">Create Campaign</a>-->
-            <a href="javascript:void(0);" id='crtTempBtn' class="crtbtn">Create Campaign</a>
+            <a href="javascript:void(0);" id='crtTempBtn' class="crtbtn">{{ Lang::get('auth.createCampaign') }}</a>
         </div>
         <div class="form-group formrow">
             <div class="selectbox">
@@ -37,7 +37,7 @@
     <div class="col-md-12 stepform3 {{ isset($campaignData->currentForm) && $campaignData->currentForm == '3' ? 'currentForm':''}}" id="3">
         <div class="form-group formrow rediobtn">
             <div class="col-md-6">
-                <input id="radio1" type="radio" name="selectList" value="customList" checked="{{isset($campaignData->selectList) &&  $campaignData->selectList == '' ? '' : 'checked'}}{{isset($campaignData->selectList) && $campaignData->selectList == 'customList' ? 'checked':''}}" ><label for="radio1">Custom List :</label>
+                <input id="radio1" type="radio" name="selectList" value="customList" checked="{{isset($campaignData->selectList) &&  $campaignData->selectList == '' ? '' : 'checked'}}{{isset($campaignData->selectList) && $campaignData->selectList == 'customList' ? 'checked':''}}" ><label for="radio1">{{ Lang::get('auth.customlist') }} :</label>
             </div>
             <div class="col-md-6">
                 <div class="selectbox">
@@ -49,18 +49,18 @@
         </div>
         <div class="form-group formrow rediobtn">
             <div class="col-md-12">
-                <input id="radio2" type="radio" name="selectList" value="quickList" checked="{{isset($campaignData->selectList) && $campaignData->selectList == 'quickList' ? 'checked':''}}" ><label for="radio2">Quick List</label>
+                <input id="radio2" type="radio" name="selectList" value="quickList" checked="{{isset($campaignData->selectList) && $campaignData->selectList == 'quickList' ? 'checked':''}}" ><label for="radio2">{{ Lang::get('auth.quicklist') }}</label>
             </div>
             <div class="quiklistblock">
                 <div class="form-group quiklistrow">
                     <div class="col-md-1"></div>
                     <div class="col-md-5">
-                        <label>Recipients gender</label>
+                        <label>{{ Lang::get('auth.recgender')}}</label>
                     </div>
                     <div class="col-md-6">
                         <div class="selectbox">
                             <i class="fa fa-caret-down droparrow"></i>
-                            {!!  Form::select('gender', array('both' => 'Male & Female','male' => 'Male', 'female' => 'Female') , null, ['id'=>'gender','tabindex'=>'6']) !!}
+                            {!!  Form::select('gender', array('both' => Lang::get("auth.malefemale"),'male' => Lang::get("auth.male"), 'female' => Lang::get("auth.female")) , null, ['id'=>'gender','tabindex'=>'6']) !!}
 
                         </div>
                     </div>
@@ -68,7 +68,7 @@
                 <div class="form-group quiklistrow">
                     <div class="col-md-1"></div>
                     <div class="col-md-5">
-                        <label>Recipients’ age range</label>
+                        <label>{{ Lang::get('auth.agerange')}}</label>
                     </div>
                     <div class="col-md-6">
                         <div class="ageblock">
@@ -82,7 +82,7 @@
                 <div class="row fliterrow quiklistrow">
                     <div class="col-md-1"></div>
                     <div class="col-md-5">
-                        <label>Router :</label>
+                        <label>{{ Lang::get('auth.router')}} :</label>
 
                     </div>
                     <div class="col-md-6">
@@ -97,7 +97,7 @@
                 <div class="form-group quiklistrow">
                     <div class="col-md-1"></div>
                     <div class="col-md-5">
-                        <label>Number of visits</label>
+                        <label>{{ Lang::get('auth.visits')}}</label>
                     </div>
                     <div class="col-md-6">
                         <div class="ageblock">
@@ -110,7 +110,7 @@
                 <div class="form-group quiklistrow">
                     <div class="col-md-1"></div>
                     <div class="col-md-5">
-                        <label>During the last</label>
+                        <label>{{ Lang::get('auth.during')}}</label>
                     </div>
                     <div class="col-md-6">
                         <div class="ageblock">
@@ -122,7 +122,7 @@
                                 <input type="hidden" name="datequickselection" value="" id="datequickselection">
                                 <div class="selectbox">
                                     <i class="fa fa-caret-down droparrow"></i>
-                                    {!!  Form::select('noOfDays', array('1' => 'Day(s)','7' => 'Week(s)', '30' => 'Month(s)','365' => 'year(s)'), null , ['id'=>'noOfDays','tabindex'=>'11']) !!}
+                                    {!!  Form::select('noOfDays', array('1' => Lang::get("auth.day"),'7' => Lang::get("auth.week"), '30' =>Lang::get("auth.month"),'365' => Lang::get("auth.year")), null , ['id'=>'noOfDays','tabindex'=>'11']) !!}
                                 </div>
                             </div>
                         </div>
@@ -151,7 +151,7 @@
     </div>
     <div class="col-md-12 stepform4 {{ isset($campaignData->currentForm) && $campaignData->currentForm == '4' ? 'currentForm':''}}" id="4">
         <div class="form-group formrow">
-            <label>Confirm yours options</label>
+            <label>{{ Lang::get('auth.confirmoption')}}</label>
             {!!  Form::hidden('templatePreview', null, array('id'=>'templatePreviewHidden')) !!}
 
             <div class="confimgblk">
@@ -159,11 +159,11 @@
             </div>
         </div>
         <div class="form-group formrow sendformrow">
-            <label>Send a test</label>
+            <label>{{ Lang::get('auth.sendtest')}}</label>
             <div class="form-group">
                 <div class="sendbox">
-                    <input class="form-control" type="email" placeholder="Enter an email address" name="testEmailAddress" id="testEmailAddress"/>
-                    <a href="javascript:void(0)" id="sendTestAddress">Send</a>
+                    <input class="form-control" type="email" placeholder="{{ Lang::get('auth.enteremail')}}" name="testEmailAddress" id="testEmailAddress"/>
+                    <a href="javascript:void(0)" id="sendTestAddress">{{ Lang::get('auth.send')}}</a>
                 </div>
             </div>
         </div>
@@ -171,9 +171,9 @@
     <div class="col-md-12"></div>
     <div class="col-md-12">
         <div class="stepbtn">
-            <button class="backbtnaa" type="submit" tabindex="13"><i><img src="{{asset("img/savebtn.png")}}" /></i> Save &amp; Exit</button>
-            <a href="javascript:void(0)" class="backbtn" tabindex="14"> <i><img src="{{asset("img/backicon.png")}}" /></i> Back </a>
-            <a href="javascript:void(0)" class="nextbtn" tabindex="15"><i><img src="{{asset("img/sendallicon.png")}}" /></i><span class="nxtButton">Next </span></a>
+            <button class="backbtnaa" type="submit" tabindex="13"><i><img src="{{asset("img/savebtn.png")}}" /></i>{{ Lang::get('auth.savequite')}}</button>
+            <a href="javascript:void(0)" class="backbtn" tabindex="14"> <i><img src="{{asset("img/backicon.png")}}" /></i> {{ Lang::get('auth.back')}} </a>
+            <a href="javascript:void(0)" class="nextbtn" tabindex="15"><i><img src="{{asset("img/sendallicon.png")}}" /></i><span class="nxtButton">{{ Lang::get('auth.next')}} </span></a>
 
         </div>
     </div>
