fe435a50e507f2827650010771d998685d7b9955
pritesh13 <garachankur@yahoo.in>
2016-02-22 18:59:30 +0530

    add lang code to transper to other lang

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -2,5 +2,6 @@
     <div class="pull-right hidden-xs">
         <b>Version</b> 1.0
     </div>
-    <strong>Copyright &copy; 2015-16 <a href="#">Clicspot</a> All rights reserved.</strong>
+<!--    <strong>Copyright &copy; 2015-16 <a href="#">Clicspot</a> All rights reserved.</strong>-->
+    <strong>{{ Lang::get('auth.copyright') }}</strong>
 </div>
