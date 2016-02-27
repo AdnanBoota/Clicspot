37d9b06426e1f68c9e816df46ad7c949f55e2205
pritesh13 <garachankur@yahoo.in>
2016-02-27 19:41:52 +0530

    browser detect language

--- app/Http/Middleware/BeforeMiddleware.php
+++ app/Http/Middleware/BeforeMiddleware.php
@@ -15,9 +15,28 @@ class BeforeMiddleware {
 	public function handle($request, Closure $next)
 	{
 //        echo Session::get('locale'); 
-         App::setLocale(Session::get('locale'));
-                
-
+         //App::setLocale(Session::get('locale'));
+          //$browser_lang = substr(Request::server('HTTP_ACCEPT_LANGUAGE'), 0, 2);
+        $langArr=array("en","fr");
+        $languages = explode(',',$_SERVER['HTTP_ACCEPT_LANGUAGE']);
+        //Session::flush();
+     
+//        if(in_array($languages[0], $langArr))
+//              App::setLocale($languages[0]);
+        
+        if(Session::has('locale')){
+           App::setLocale(Session::get('locale')); 
+        }
+        else
+        {
+            if(in_array($languages[0], $langArr))
+              App::setLocale($languages[0]);
+        }
+        
+        
+        
+//        print_r($languages);
+         //exit;
 		return $next($request);
 	}
 
