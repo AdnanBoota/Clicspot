1251b2554567652799db9ea3eeb35f86db89bb74
mitulgadhia <gadhia.mitul41@gmail.com>
2016-03-02 02:16:21 +0530

    ssh

--- app/Http/Controllers/RouterControllerAR71XX.php
+++ app/Http/Controllers/RouterControllerAR71XX.php
@@ -14,6 +14,12 @@ class RouterControllerAR71XX extends Controller
      *
      * @return Response
      */
+
+    public function __construct()
+    {
+        $this->middleware(['except' => ['BeforeMiddleware']]);
+    }
+
     public function index()
     {
         return "Welcome to API v1";
--- app/Http/Middleware/BeforeMiddleware.php
+++ app/Http/Middleware/BeforeMiddleware.php
@@ -3,41 +3,32 @@
 use Closure;
 use App;
 use Session;
-class BeforeMiddleware {
 
-	/**
-	 * Handle an incoming request.
-	 *
-	 * @param  \Illuminate\Http\Request  $request
-	 * @param  \Closure  $next
-	 * @return mixed
-	 */
-	public function handle($request, Closure $next)
-	{
-//        echo Session::get('locale'); 
-         //App::setLocale(Session::get('locale'));
-          //$browser_lang = substr(Request::server('HTTP_ACCEPT_LANGUAGE'), 0, 2);
-        $langArr=array("en","fr");
-        $languages = explode(',',$_SERVER['HTTP_ACCEPT_LANGUAGE']);
-        //Session::flush();
-     
-//        if(in_array($languages[0], $langArr))
-//              App::setLocale($languages[0]);
-        
-        if(Session::has('locale')){
-           App::setLocale(Session::get('locale')); 
+class BeforeMiddleware
+{
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $langArr = array("en", "fr");
+        if (isset($_SERVER['HTTP_ACCEPT_LANGUAGE'])) {
+            $languages = explode(',', $_SERVER['HTTP_ACCEPT_LANGUAGE']);
+        } else {
+            $languages[0] = "en";
         }
-        else
-        {
-            if(in_array($languages[0], $langArr))
-              App::setLocale($languages[0]);
+        if (Session::has('locale')) {
+            App::setLocale(Session::get('locale'));
+        } else {
+            if (in_array($languages[0], $langArr))
+                App::setLocale($languages[0]);
         }
-        
-        
-        
-//        print_r($languages);
-         //exit;
-		return $next($request);
-	}
+        return $next($request);
+    }
 
 }
